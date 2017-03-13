77655c04b0cb3a3a800bca987fdd7ee45faa5ad7
carloscalderon <cdcalderon@gmail.com>
2017-03-13 01:56:24 -0500

    inti commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	[SerializeField] private GameObject mainMenu;
+
+	private bool playerActive = false;
+	private bool gameOver = false;
+	private bool gameStarted = false;
+
+	public bool PlayerActive {
+		get { return playerActive; }
+	}
+
+	public bool GameOver {
+		get { return gameOver; }
+	}
+
+	public bool GameStarted {
+		get { return gameStarted; }
+	}
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		Assert.IsNotNull (mainMenu);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayerCollided() {
+		gameOver = true;
+	}
+
+	public void PlayerStartedGame() {
+		playerActive = true;
+	}
+
+	public void EnterGame() {
+		mainMenu.SetActive (false);
+		gameStarted = true;
+	}
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	[SerializeField] private float objectSpeed = 1;
+	[SerializeField] private float resetPosition = -21.65f;
+	[SerializeField] private float startPosition = 79.67f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+
+		if (!GameManager.instance.GameOver) {
+			transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+			if (transform.localPosition.x <= resetPosition) {
+				Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+				transform.position = newPos;
+			}
+		}
+
+	}
+}
