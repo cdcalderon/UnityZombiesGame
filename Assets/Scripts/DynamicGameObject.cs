c197bc8b0d7c83a01a5f0bfc3077ec681eea4d3d
carloscalderon <cdcalderon@gmail.com>
2017-03-15 01:10:01 -0500

    implement rock movement and rosk cordination routine

---
+++ Assets/Scripts/DynamicGameObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DynamicGameObject : MonoBehaviour {
+
+  [SerializeField] private float objectSpeed = 1;
+  [SerializeField] private float resetPosition = -21.65f;
+  [SerializeField] private float startPosition = 79.67f;
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  protected virtual void Update () {
+
+    // if (!GameManager.instance.GameOver) {
+    // 	transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+    // 	if (transform.localPosition.x <= resetPosition) {
+    // 		Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+    // 		transform.position = newPos;
+    // 	}
+    // }
+    transform.Translate (Vector3.left * (objectSpeed * Time.deltaTime));
+
+    if (transform.localPosition.x <= resetPosition) {
+      Vector3 newPos = new Vector3 (startPosition, transform.position.y, transform.position.z);
+      transform.position = newPos;
+    }
+
+  }
+}
